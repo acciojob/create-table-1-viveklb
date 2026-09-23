db4791d53401e01ed8105d1f89022ee950482930
vivek bharamshetti <69708672+viveklb@users.noreply.github.com>
2026-09-23 12:24:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+  const table = document.getElementById("sampleTable");
+
+  const newRow = table.insertRow(0);
+
+  const cell1 = newRow.insertCell(0);
+  const cell2 = newRow.insertCell(1);
+
+  cell1.innerHTML = "New Cell1";
+  cell2.innerHTML = "New Cell2";
 }
